2a62beed23e15a84394b5da161dfc42dfe0c64db
Shreejal Dhungana <dshreejal@gmail.com>
2022-12-02 21:39:09 +0545

    photo upload using multer

--- index.js
+++ index.js
@@ -7,16 +7,16 @@ connectToMongo();
 require('dotenv/config');
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 5050;
 
 app.use(cors());
 app.use(express.json());
-
+app.use("/images", express.static("./images"))
 //Available routes
 app.use('/api/auth', require("./routes/auth")) //Route for user SignUp, LogIn
 app.use('/api/blog', require("./routes/blog")) //Route for Blog- Create, Fetch, Edit, Delete
 
 app.listen(port, () => {
-    console.log(`blog-site backend deployed on port http://localhost:${port}`);
+    console.log(`blog-site backend deployed on port ${port}`);
 })
 
