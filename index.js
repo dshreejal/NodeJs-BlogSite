55fa85a9297ea726474871f7c29186dfa50e404f
Shreejal Dhungana <dshreejal@gmail.com>
2023-03-11 21:45:46 +0545

    static images folder

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const connectToMongo = require("./db");
 const express = require("express");
 const cors = require("cors");
-
+const path = require('path');
 connectToMongo();
 
 require('dotenv/config');
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5050;
 
 app.use(cors());
 app.use(express.json());
-app.use("/images", express.static("./images"))
+app.use("/images/", express.static(path.join(__dirname, "/images")))
 //Available routes
 app.use('/api/auth', require("./routes/auth")) //Route for user SignUp, LogIn
 app.use('/api/blog', require("./routes/blog")) //Route for Blog- Create, Fetch, Edit, Delete
